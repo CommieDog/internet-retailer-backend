6455f6a8277eb2224e49e92c0b98b1fcc7a88868
John Netzel <johnrnetzel@gmail.com>
2022-05-04 22:27:20 -0800

    Add GET api/tags/ routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,26 +3,47 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
   // find all tags
+router.get('/', async (req, res) => {
   // be sure to include its associated Product data
+  const tags = await Tag.findAll(
+    {
+      include:
+      [
+        {
+          model: Product,
+        },
+      ],
+    },
+  );
+  res.json(tags);
 });
 
-router.get('/:id', (req, res) => {
   // find a single tag by its `id`
-  // be sure to include its associated Product data
+router.get('/:id', async (req, res) => {
+  const tag = await Tag.findByPk(req.params.id,
+  {
+    include:
+    [
+      {
+        model: Product,
+      },
+    ],
+  },
+);
+res.json(tag);
 });
 
-router.post('/', (req, res) => {
   // create a new tag
+router.post('/', async (req, res) => {
 });
 
-router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+router.put('/:id', async (req, res) => {
 });
 
-router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+router.delete('/:id', async (req, res) => {
 });
 
 module.exports = router;
