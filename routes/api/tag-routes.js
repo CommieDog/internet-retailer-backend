433b80bd4390b358a202579532aa0dffa91f5248
John Netzel <johnrnetzel@gmail.com>
2022-05-05 08:52:21 -0800

    Add api/tags/ routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -36,14 +36,56 @@ res.json(tag);
 
   // create a new tag
 router.post('/', async (req, res) => {
+  const newTag = await Tag.create(
+  {
+    tag_name: req.body.tagName,
+  });
+res.json(newTag);
 });
 
   // update a tag's name by its `id` value
 router.put('/:id', async (req, res) => {
+  let updatedTag = await Tag.update(
+    {
+      tag_name: req.body.tagName
+    },
+    {
+      where:
+      {
+        id: req.params.id
+      },
+      plain: true,
+    }
+  );
+  if(updatedTag[0] === 0)
+  {
+    return res.json({}); // No update, bail and return empty object
+  }
+  updatedTag = await Tag.findByPk(req.params.id, {
+    plain: true
+  });
+  res.json(updatedTag);
 });
 
   // delete on tag by its `id` value
 router.delete('/:id', async (req, res) => {
+  await ProductTag.destroy(
+    {
+      where:
+      {
+        tag_id: req.params.id
+      }
+    }
+  );
+  const deletedTag = await Tag.destroy(
+    {
+      where:
+      {
+        id: req.params.id
+      }
+    }
+  );
+  res.json(deletedTag);
 });
 
 module.exports = router;
